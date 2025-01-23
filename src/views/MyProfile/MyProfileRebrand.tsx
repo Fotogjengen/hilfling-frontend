f57df050ac81c58d57a3cf1fae7e5aaf882d3beb
Mathilde1440 <mathilde.skaset.haarr@gmail.com>
2025-01-23 20:53:05 +0100

    Create interface user to store user information fetched from API

--- src/views/MyProfile/MyProfileRebrand.tsx
+++ src/views/MyProfile/MyProfileRebrand.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
+interface user {
+    name : string ; 
+    userName : string;
+    adress : string ;
+    zip : number;
+    city : string;
+    phoneNumber : number;
+    eMail: string ;
+    samfundetEMail : string ; 
+    currentPosition : string | undefined ;  
+    formerPositions : any  | undefined;
+    role : string | undefined // Is the person a web-developer or a photographer
+    admissionSemester : string ;
+   
+}
+
+
 const MyProfileRebrand = () => {
   return (
     <div>MyProfileRebrand</div>
