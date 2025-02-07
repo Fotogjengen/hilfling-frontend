9ae1146374c54058173de89b105915b6edad6bd3
Mathilde1440 <mathilde.skaset.haarr@gmail.com>
2025-02-07 18:36:06 +0100

    Create mock data

--- src/views/MyProfile/MyProfileRebrand.tsx
+++ src/views/MyProfile/MyProfileRebrand.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { User } from './mockdata';
 
 interface user {
     name : string ; 
@@ -9,15 +10,39 @@ interface user {
     phoneNumber : number;
     eMail: string ;
     samfundetEMail : string ; 
-    currentPosition : string | undefined ;  
-    formerPositions : any  | undefined;
-    role : string | undefined // Is the person a web-developer or a photographer
-    admissionSemester : string ;
+    currentPosition? : string | undefined ;  
+    formerPositions? : string[] | undefined;
+    role? : string | undefined // "Fotograf" or "Web"
+    admissionSemester? : string ;
    
 }
 
+const mockUser:user = {  // Mock-user for test purpose
+  name : "Hallgeir Kvadsheim",
+  userName : "Brukernavn : HallKva",
+  adress : "Herman Krags vei 13",
+  zip : 7051,
+  city : "Trondheim",
+  phoneNumber : 4712345657,
+  eMail: "Hallgair.K@gmail.com" ,
+  samfundetEMail : "Hallkva@samfundet.no" ,
+  currentPosition :  "økonomisjef",
+  formerPositions : ["Miljøbilde", "Nyoptatt"],
+  
+  admissionSemester : "h25"
+ 
+};
+
+const webPositions: string[] = [ "Websjef", "Benkmester", "Opplæringsannsvarlig", "WebAdmin"];
+
 
 const MyProfileRebrand = () => {
+
+  //Make api-call
+  //determine if web or fotograf
+  //push api-info into + role into "user" variable
+ 
+
   return (
     <div>MyProfileRebrand</div>
   )
