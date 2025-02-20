e5698d3a891c1cc5ee82e84070cc12ff484adbc9
Mathilde1440 <mathilde.skaset.haarr@gmail.com>
2025-02-20 16:50:58 +0100

    Fix api call to display user information

--- src/views/MyProfile/MyProfileRebrand.tsx
+++ src/views/MyProfile/MyProfileRebrand.tsx
@@ -1,49 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import "./MyProfileRebrand.css"
+import axios from 'axios';
 
 //https:\/\/images.dog.ceo\/breeds\/dane-great\/n02109047_6042.jpg
 
 
 interface UserInfo {
-    name : string ; 
+    profilePicure : string;
+    firstName : string ; 
+    lastName : string;
     userName : string;
     adress : string ;
     zip : string;
     city : string;
     phoneNumber : string;
-    eMail: string ;
+    // eMail: string ;
     samfundetEMail : string ; 
-    currentPosition : string | "";  
-    formerPositions : string[] | [""];
-    role? : string | "" // "Fotograf" or "Web"
+    currentPosition : string | " ";  
+    formerPositions : string[] | [" "];
+    role? : string | " " // "Fotograf" or "Web"
     admissionSemester? : string ;
    
 }
 
-const mockUser:UserInfo = {  // Mock-user for test purpose
-  name : "Hallgeir Kvadsheim",
-  userName : "Brukernavn : HallKva",
-  adress : "Herman Krags vei 13",
-  zip : "7051",
-  city : "Trondheim",
-  phoneNumber : "4712345657",
-  eMail: "Hallgair.K@gmail.com" ,
-  samfundetEMail : "Hallkva@samfundet.no" ,
-  currentPosition :  "Økonomisjef",
-  formerPositions : ["Miljøbilde", "Nyoptatt"],
+// const mockUser:UserInfo = {  // Mock-user for test purpose
+//   profilePicure : "https://media1.tenor.com/images/79f8be09f39791c6462d30c5ce42e3be/tenor.gif?itemid=18386674",
+//   firstName : "Hallgeir",
+//   lastName: "Kvadsheim",
+//   userName : "Brukernavn : HallKva",
+//   adress : "Herman Krags vei 13",
+//   zip : "7051",
+//   city : "Trondheim",
+//   phoneNumber : "4712345657",
+//   // eMail: "Hallgair.K@gmail.com" ,
+//   samfundetEMail : "Hallkva@samfundet.no" ,
+//   currentPosition :  "Økonomisjef",
+//   formerPositions : ["Miljøbilde", "Nyoptatt"],
   
-  admissionSemester : "Høst 25"
+//   admissionSemester : "Høst 25"
  
-};
+// };
 
 const emptyUser:UserInfo = {
-  name : " ",
+  profilePicure : "https://media1.tenor.com/images/79f8be09f39791c6462d30c5ce42e3be/tenor.gif?itemid=18386674" ,
+  firstName : " ",
+  lastName : " ",
   userName : " ",
   adress : " ",
   zip : " ",
   city : " ",
   phoneNumber : " ",
-  eMail: " " ,
+  // eMail: " " ,
   samfundetEMail : " " ,
   currentPosition :  " ",
   formerPositions : [" "],
@@ -52,48 +59,80 @@ const emptyUser:UserInfo = {
 
 };
 
-// const webPositions: string[] = [ "Websjef", "Benkmester", "Opplæringsannsvarlig", "WebAdmin"];
+
+
+//http://localhost:8000/photo_gang_bangers/7a89444f-25f6-44d9-8a73-94587d72b839
 
 
 const MyProfileRebrand = () => {
 
+  const webPositions: string[] = [ "websjef", "benkmester", "opplæringsannsvarlig", "webAdmin", "web"];
+
   const [currentUser, setCurrentUser] = useState<UserInfo>(emptyUser);
 
-  const dogPicture = "https://images.dog.ceo/breeds//dane-great/n02109047_6042.jpg";  //Placeholder profile_pic
-
-  //Make api-call
-  //determine if web or fotograf
-  //push api-info into + role into "user" variable
   useEffect (() => {
     const getUser = () => {
-    
-    const cUser = mockUser;
-    cUser.role = "Fotograf";
-    setCurrentUser(cUser);
+  
+    //Make api-call
+      const url = "http://localhost:8000/photo_gang_bangers/7a89444f-25f6-44d9-8a73-94587d72b839" //For testing purpose. Has to be modified make corrrect api call
+
+      axios
+      .get(url)
+      
+      .then((response) => {
+        const profilePicure = response.data.samfundetUser.profilePicturePath;
+        const firstName = response.data.samfundetUser.firstName;
+        const lastName = response.data.samfundetUser.lastName;
+        const userName =  response.data.samfundetUser.username;
+        const adress = response.data.address;
+        const zip = response.data.zipCode;
+        const city = response.data.city;
+        const phoneNumber = response.data.samfundetUser.phoneNumber.value;
+        // const eMail= response.data.samfundetUser.email.value;
+        const samfundetEMail = response.data.samfundetUser.email.value; 
+        const currentPosition =  response.data.position.title;
+        const formerPositions = [" "]; //Not supported yet
+        const admissionSemester = response.data.semesterStart.value;
+            //determine if web or fotograf
+        const role = "Fotograf";
+        // console.log(webPositions.includes(currentUser.currentPosition.toLowerCase()))
+
+        setCurrentUser({profilePicure, firstName, lastName, userName, adress,zip, city, phoneNumber, samfundetEMail, currentPosition, formerPositions, role, admissionSemester}); 
+      })
+      
+      .catch((error) => {
+
+        console.error("Error fetching user:", error); 
+      });
+
+      if (webPositions.includes(currentUser.currentPosition.toLowerCase()) ){
+        setCurrentUser((previous) => ({...previous, role: "Web"}));
+        
+      }
 
     };
 
   getUser();
-    }, []);
+    }, [currentUser]);
 
   return (
     <>
     <div className= 'main_card' >
 
       <header className = "name_display" >
-        {currentUser?.name || "loading ... "}
+        {(currentUser?.firstName + " " + currentUser?.lastName) || "loading ... "}
         <h2 className = "role_display" > 
           {currentUser?.role || "loading ... "}
         </h2>
       </header>
     
       <div className = 'info_card'>
-        
+
         <div className = 'card_1'> {/* Contains profile picture and personal info*/}
 
           <div className = 'profile_picture' >
             <div className = 'profile_picture_img'>
-              <img src={dogPicture} alt="Profile" height="225" width="225" /> 
+              <img src={currentUser.profilePicure} alt="Profile" height="225" width="225" /> 
             </div>
           </div>
 
@@ -113,47 +152,10 @@ const MyProfileRebrand = () => {
 
           </div>
 
-          {/* <div className = 'personal_information' >
-            <h1 className = 'personal_information_header'> 
-              {"Personlig informasjon"}
-            </h1>
-            <h2 className= 'personal_information_list'> 
-              <div>  
-                {currentUser.userName}  
-              </div>
-              <div>  
-                {"Samfundet e-post: " + currentUser.samfundetEMail}
-              </div>
-              <div>  
-                {"e-post: " + currentUser.eMail}
-              </div>
-              <div>  
-                {"Telefon: " + currentUser.phoneNumber}
-              </div>
-              <div>  
-                {"Adresse: " + currentUser.adress + ", " + currentUser.zip + " " + currentUser.city}
-              </div>
-            </h2>
-          </div> */}
 
         </div>
 
         <div className = 'card_2'> {/* Contains position and admission semester*/}
-
-          {/* <div className = 'positions'>
-          <h1 className = 'positions_header'> 
-              {"Verv"}
-            </h1>
-            <h2 className = 'positions_list'> 
-              {currentUser?.currentPosition || "Loading..."}
-              <div>{currentUser?.formerPositions.map((position, index) => (
-                <div key = {index} >
-                    {position}
-                </div>
-                  ))}
-              </div>
-            </h2>
-          </div> */}
 
           <div className = 'personal_information' >
             <h1 className = 'personal_information_header'> 
@@ -161,14 +163,14 @@ const MyProfileRebrand = () => {
             </h1>
             <h2 className= 'personal_information_list'> 
               <div>  
-                {currentUser.userName}  
+                {"Brukernavn: " + currentUser.userName}  
               </div>
               <div>  
                 {"Samfundet e-post: " + currentUser.samfundetEMail}
               </div>
-              <div>  
+              {/* <div>  
                 {"e-post: " + currentUser.eMail}
-              </div>
+              </div> */}
               <div>  
                 {"Telefon: " + currentUser.phoneNumber}
               </div>
