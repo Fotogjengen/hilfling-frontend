f3e141b7a22dc8e34f38eb1f7b507d6800d59a7a
Philip Torvund Bennett <philitb@stud.ntnu.no>
2025-06-29 20:37:35 +0200

    Added pagination suport for album

--- src/views/Intern/Arkivsjef/ArchiveBoss.tsx
+++ src/views/Intern/Arkivsjef/ArchiveBoss.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useContext, useEffect, useState } from "react";
 import styles from "./Arkivsjef.module.css";
 import ArchiveBossAccordion from "../../../components/Arkivsjef/ArchiveBossAccordion/ArchiveBossAccordion";
-import { Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Pagination, Typography } from "@mui/material";
 import { AlbumDto, PlaceDto, CategoryDto } from "../../../../generated";
 import { AlbumApi } from "../../../utils/api/AlbumApi";
 import { PlaceApi } from "../../../utils/api/PlaceApi";
@@ -13,15 +13,41 @@ import { AlertContext, severityEnum } from "../../../contexts/AlertContext";
 import ArchiveBossCreateUsers from "../../../components/Arkivsjef/ArchiveBossCreateUser/ArchiveBossCreateUsers";
 import ArchiveBossOverView from "../../../components/Arkivsjef/ArchiveBossOverView/ArchiveBossOverView";
 
+interface PaginatedData<T> {
+  items: T[];
+  totalCount: number;
+  totalPages: number;
+}
+
 const ArchiveBoss: FC = () => {
   const [albums, setAlbums] = useState<AlbumDto[]>([]);
   const [places, setPlaces] = useState<PlaceDto[]>([]);
-  const [update, setUpdate] = useState(false);
   const [categories, setCategories] = useState<CategoryDto[]>([]);
+
+  const [albumsPage, setAlbumsPage] = useState(1);
+  const [placesPage, setPlacesPage] = useState(1);
+  const [categoriesPage, setCategoriesPage] = useState(1);
+
+  const [albumsTotalPages, setAlbumsTotalPages] = useState(1);
+  const [placesTotalPages, setPlacesTotalPages] = useState(1);
+  const [categoriesTotalPages, setCategoriesTotalPages] = useState(1);
+
+  const [update, setUpdate] = useState(false);
   const [createUser, setCreateUser] = useState(false);
   const [overview, setOverview] = useState(false);
 
+  const [loading, setLoading] = useState<{
+    albums: boolean;
+    places: boolean;
+    categories: boolean;
+  }>({
+    albums: false,
+    places: false,
+    categories: false,
+  });
+
   const { setMessage, setSeverity, setOpen } = useContext(AlertContext);
+  const itemsPerPage = 6;
 
   const setError = (e: string) => {
     setOpen(true);
@@ -29,23 +55,116 @@ const ArchiveBoss: FC = () => {
     setMessage(e);
   };
 
+  const fetchAlbums = async (page: number) => {
+    setLoading((prev) => ({ ...prev, albums: true }));
+    await AlbumApi.getAll({
+      page: page - 1,
+      pageSize: itemsPerPage,
+    })
+      .then((res) => {
+        setAlbums(res.data.currentList);
+        setAlbumsTotalPages(res.data.totalPages);
+      })
+      .catch((err) => {
+        setError(err as string);
+      })
+      .finally(() => {
+        setLoading((prev) => ({ ...prev, albums: false }));
+      });
+  };
+
+  // const fetchPlaces = async (page: number) => {
+  //   setLoading((prev) => ({ ...prev, places: true }));
+  //   try {
+  //     const res = await PlaceApi.getAll({
+  //       page: page - 1,
+  //       pageSize: itemsPerPage,
+  //     });
+
+  //     setPlaces(res.data.currentList);
+  //     setPlacesTotalPages(res.data.totalPages);
+  //   } catch (e) {
+  //     setError(e as string);
+  //   } finally {
+  //     setLoading((prev) => ({ ...prev, places: false }));
+  //   }
+  // };
+
+  // const fetchCategories = async (page: number) => {
+  //   setLoading((prev) => ({ ...prev, categories: true }));
+  //   try {
+  //     const res = await CategoryApi.getAll({
+  //       page: page - 1,
+  //       pageSize: itemsPerPage,
+  //     });
+
+  //     setCategories(res.data.currentList);
+  //     setCategoriesTotalPages(res.data.totalPages);
+  //   } catch (e) {
+  //     setError(e as string);
+  //   } finally {
+  //     setLoading((prev) => ({ ...prev, categories: false }));
+  //   }
+  // };
+
+  const handleAlbumsPageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
+    setAlbumsPage(value);
+    void fetchAlbums(value);
+  };
+
+  const handlePlacesPageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
+    setPlacesPage(value);
+    // fetchPlaces(value);
+  };
+
+  const handleCategoriesPageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
+    setCategoriesPage(value);
+    // fetchCategories(value);
+  };
+
   useEffect(() => {
-    AlbumApi.getAll()
-      .then((res) => setAlbums(res.data.currentList))
-      .catch((e) => {
-        setError(e);
-      });
-    PlaceApi.getAll()
-      .then((res) => setPlaces(res.data.currentList))
-      .catch((e) => {
-        setError(e);
-      });
-    CategoryApi.getAll()
-      .then((res) => setCategories(res.data.currentList))
-      .catch((e) => {
-        setError(e);
-      });
+    void fetchAlbums(1);
+    // fetchPlaces(1);
+    // fetchCategories(1);
   }, []);
+
+  useEffect(() => {
+    if (update) {
+      void fetchAlbums(albumsPage);
+      // fetchPlaces(placesPage);
+      // fetchCategories(categoriesPage);
+      setUpdate(false);
+    }
+  }, [update, albumsPage, placesPage, categoriesPage]);
+
+  // useEffect(() => {
+  //   AlbumApi.getAll({ page: 0, pageSize: 2 })
+  //     .then((res) => {
+  //       setAlbums(res.data.currentList);
+  //     })
+  //     .catch((e) => {
+  //       setError(e);
+  //     });
+  //   PlaceApi.getAll()
+  //     .then((res) => setPlaces(res.data.currentList))
+  //     .catch((e) => {
+  //       setError(e);
+  //     });
+  //   CategoryApi.getAll()
+  //     .then((res) => setCategories(res.data.currentList))
+  //     .catch((e) => {
+  //       setError(e);
+  //     });
+  // }, []);
 
   return (
     <>
@@ -77,20 +196,17 @@ const ArchiveBoss: FC = () => {
             <Grid
               container
               direction="row"
-              display = "flex"
+              display="flex"
               justifyContent="start"
               alignItems="center"
               spacing={"2rem"}
               flexWrap="wrap"
               padding={"1rem"}
             >
-             <Grid item xs={12} sm={4} md="auto">
+              <Grid item xs={12} sm={4} md="auto">
+                <ArchiveBossAddElements />
+              </Grid>
 
-              <ArchiveBossAddElements />
-                    
-            </Grid>
-      
-    
               <Grid item xs={12} sm={9}>
                 <Typography>
                   Denne siden er for fotogjengens Arkivsjef. Her kan du legge
@@ -104,20 +220,36 @@ const ArchiveBoss: FC = () => {
           </div>
 
           <ArchiveBossAccordion color="#BE3144" name="Album">
-            <Grid container spacing={2}>
-              {albums.map((album: AlbumDto, index: number) => (
-                <Grid item key={index} xs={12} sm={4}>
-                  <ArchiveBossElement
-                    text={album.title}
-                    id={album.albumId.id}
-                    key={index}
-                    type="album"
-                  />
+            {loading.albums ? (
+              <Typography>Laster albums...</Typography>
+            ) : (
+              <>
+                <Grid container spacing={2}>
+                  {albums.map((album: AlbumDto, index: number) => (
+                    <Grid item key={index} xs={12} sm={4}>
+                      <ArchiveBossElement
+                        text={album.title}
+                        id={album.albumId.id}
+                        key={index}
+                        type="album"
+                      />
+                    </Grid>
+                  ))}
                 </Grid>
-              ))}
-            </Grid>
+                <Box display="flex" justifyContent="center" mt={2}>
+                  <Pagination
+                    count={albumsTotalPages}
+                    page={albumsPage}
+                    onChange={handleAlbumsPageChange}
+                    color="primary"
+                  />
+                </Box>
+              </>
+            )}
           </ArchiveBossAccordion>
-          <ArchiveBossAccordion color="#8F4650" name="Sted"> {/*#605C5C*/}  {/*#7C3640*/}
+          <ArchiveBossAccordion color="#8F4650" name="Sted">
+            {" "}
+            {/*#605C5C*/} {/*#7C3640*/}
             <Grid container spacing={2}>
               {places.map((place: PlaceDto, index: number) => (
                 <Grid item key={index} xs={12} sm={4}>
@@ -131,7 +263,9 @@ const ArchiveBoss: FC = () => {
               ))}
             </Grid>
           </ArchiveBossAccordion>
-          <ArchiveBossAccordion color= "#605C5C" name="Kategori">  {/*#3A3B3C*/}
+          <ArchiveBossAccordion color="#605C5C" name="Kategori">
+            {" "}
+            {/*#3A3B3C*/}
             <Grid container spacing={2}>
               {categories.map((category: CategoryDto, index: number) => (
                 <Grid item key={index} xs={12} sm={4}>
