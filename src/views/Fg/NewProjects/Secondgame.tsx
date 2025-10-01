1ea25b26049b5134a28c9a8b0297bb2cafc58df3
ruby <ruben.vooren@gmail.com>
2025-10-01 21:04:45 +0200

    feat: wowowowow masse kult nytt spill

--- src/views/Fg/NewProjects/Secondgame.tsx
+++ src/views/Fg/NewProjects/Secondgame.tsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import { experimentalStyled as styled } from "@mui/material/styles";
-import { Grid, Paper, Typography } from "@mui/material";
+import { getScopedCssBaselineUtilityClass, Grid, Paper, Typography } from "@mui/material";
 import VolcanoIcon from '@mui/icons-material/Volcano';
 import { Link } from "react-router-dom";
+import './Secondgame.css';
+import { questions } from './questions';
+import { random, set } from "lodash";
 
 const Secondgame = () => {
   const IconSize = 100;
 
+  const [question, setQuestion] = useState<string>("Klar for 100 (120) questions!?!?");
+  const [everyTenth, setTenth] = useState<string>("");
+  const [splash, setSplashh] = useState<string>("");
+  const [nr, setNr] = useState<number>(1);
 
+  function nyttSpørsmål() {
+    setNr(nr + 1);
+    setQuestion(questions[random(1, 100)]);
+
+    if (nr%10 === 0 ) {
+      setTenth(`${nr} spørsmål - 🍻SKÅL🍻`);
+    } else {
+      setTenth("");
+    }
+
+    const randomSplash = random(1, 6);
+    if (randomSplash === 1) {
+      setSplashh("Splashhhh🎉🎉🎉");
+    } else {
+      setSplashh("");
+    }
+  }
 
   return (
-    <div>
-      <h1>Her er game 2</h1>
-
+    <div id="secondgame">
+      <p id="gameTitle">100 (120) questions</p>
+      <div id="container-main">
+        <div id="question-container">
+          <p>{question}</p>
+          <p>{splash}</p>
+          <p>{everyTenth}</p>
+        </div>
+        <div id="qstbutton-container">
+          <button id="qstbutton" onClick={nyttSpørsmål}>Neste</button>
+        </div>
+      </div>
     </div>
   );
 };
