b7e39f6cec432ca12cc03e83840d355adfd601fa
Mathilde <mathilde.skaset.haar@gmail.com>
2025-10-08 20:15:59 +0200

    code cleanup

--- src/components/MyProfile/EditProfilepic/EditProfilpic.tsx
+++ src/components/MyProfile/EditProfilepic/EditProfilpic.tsx
@@ -19,7 +19,6 @@ const EditProfilepic = ({ setEditProfilepic }: Props) => {
     const [noPictureUploaded, setnoPictureUploade] = useState(true);
     const [file, setFile] = useState<DragNDropFile |  null> (null); // stores the uploaded files
     const [preview, setPreview] = useState<string> ("");
-    // const navigate = useNavigate();
 
     const { acceptedFiles, getRootProps, getInputProps, open } = useDropzone({
         accept: ".jpg,.jpeg,.png",
@@ -36,41 +35,16 @@ const EditProfilepic = ({ setEditProfilepic }: Props) => {
             setFile(file_new)
             setnoPictureUploade(false)
 
-
-        
     }},[acceptedFiles])
 
     useEffect   (() => {
 
-
         if (!file) return;
         const objectUrl = URL.createObjectURL(file);
         setPreview(objectUrl);
-
-
         
     },[file])
 
-
-    //     setFile((prevFiles) => [
-    //           ...prevFiles,
-    //           ...(acceptedFiles as DragNDropFile[]).map((file) => {
-
-    //             return file;
-    //           }),
-    //         ]);
-
-    // },[acceptedFiles])
-
-    
-
-//   const handleRemoveProfilePic = (index: number) => {
-//     setFiles((prevFiles) => {
-//       const newFiles = [...prevFiles];
-//       newFiles.splice(index, 1);
-//       return newFiles;
-//     });
-//   };
     const uploadProfilePicBtn = () => {
 
         setnoPictureUploade(false);
@@ -87,72 +61,32 @@ const EditProfilepic = ({ setEditProfilepic }: Props) => {
         setEditProfilepic(false);
     }
 
-//    <div className={styles.container}>
-//         <img
-//           width={100}
-//           height={100}
-//           src={URL.createObjectURL(file)}
-//           alt={file.name}
-//           className={styles.image}
-//         />
-//     <div>
-
-    
-
 //https://foto.samfundet.no/media/alle/prod/DIGGE/digge0982.jpg
 
   return (
     <div>
         <Paper className = "pop_up_box"> {/*Main body for the pop up */}
-
-            <div className="picture_preview">   {/* Contains a preview of the profile picture uploaded or a skeleton box*/}
-                                            
+            <div className="picture_preview">   {/* Contains a preview of the profile picture uploaded or a skeleton box*/}                          
                 { !noPictureUploaded && ( 
-
-     
                         <img                        
                         src={preview}
                         alt="Profile"
                         height="400"
                         width="90%"
                         />
-
                 )}
-                
                 { noPictureUploaded && (
-
                 //  <Skeleton variant= "rectangular" width="90%" height={400}  />
                 <section >
-                    <div
-                    {...getRootProps({ className: "dropzone" })}
-                  
-                    >
-                    <input {...getInputProps()} />
-                    <p>------------------Dra og slipp filer her------------------</p>
-                    {/* <Button
-                            variant="contained"
-                            onClick={open}
-                            >
-                            Velg fil
-                    </Button> */}
+                    <div {...getRootProps({ className: "dropzone" })} >
+                        <input {...getInputProps()} />
+                            <p>------------------Dra og slipp filer her------------------</p>
                     </div>
-        
-                    {/* <aside>
-                    <ul className={styles.noStyleUl}>{renderFilePreview}</ul>
-                    </aside> */}
                 </section>
-
-
                 )}
-
-   
             </div>
-{/* <Button disabled>Disabled</Button> */}
+
             <div className="nav_buttons"> {/*Contains navigation buttons*/}
-
-
-
-
 
                 <Button disabled = {noPictureUploaded} className = 'button_styling' onClick = {useAsProfilepicture}>
                     Bruk som profilbilde
@@ -169,10 +103,6 @@ const EditProfilepic = ({ setEditProfilepic }: Props) => {
                 <Button  disabled = {noPictureUploaded} className = 'button_styling' onClick = {resteBtn}>
                     Tilbakestill bilde
                 </Button>
-
-
-                
-
 
 
             </div>
