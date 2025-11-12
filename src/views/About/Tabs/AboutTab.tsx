dc65da929d08f28267dc2dba1202626958ed9af2
Mathilde <mathilde.skaset.haar@gmail.com>
2025-11-12 17:35:35 +0100

    code cleanup

--- src/views/About/Tabs/AboutTab.tsx
+++ src/views/About/Tabs/AboutTab.tsx
@@ -20,7 +20,7 @@ const AboutTab: FC = () => {
 
     const [activeGangBangers, setActiveGangBangers] = useState<PhotoGangBangerPublicDto[]>([]);
     const [activePangs, setActivePangs] = useState<PhotoGangBangerPublicDto[]>([],);
-    const [inActivePangs, setInactivePangs] = useState<PhotoGangBangerPublicDto[]>([]);
+
 
 
 
@@ -33,9 +33,7 @@ const AboutTab: FC = () => {
     PhotoGangBangerApi.getAllActivePangsPublic()
         .then((res) => setActivePangs(res))
         .catch((err) => console.log(err));
-    PhotoGangBangerApi.getAllInactivePangsPublic()
-        .then((res) => setInactivePangs(res))
-        .catch((err) => console.log(err));
+
     }, []);
 
 
