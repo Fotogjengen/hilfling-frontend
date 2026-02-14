d5b8496c50984f4fb0fa8308354ecc38cfb39ba2
Mathilde <mathilde.skaset.haar@gmail.com>
2026-02-14 13:41:21 +0100

    Code cleanup

--- src/components/DownloadImages/CreditPopUp/CreditPopUp.tsx
+++ src/components/DownloadImages/CreditPopUp/CreditPopUp.tsx
@@ -13,20 +13,16 @@ const CreditPopUp =({ setTriggerCreditPopUp, setDownloadAbort }: Props)  => {
     const [isIntern, setIsIntern] = useState(false);
 
     useEffect( () =>{
-
         setIsIntern(true);
-
     }),[];
 
     const handleAccept = () => {
         setTriggerCreditPopUp(false);
-
     }
 
     const handleAbort = () => {
         setDownloadAbort(true);
         setTriggerCreditPopUp(false);
-        
     }
     
     return (
@@ -36,40 +32,28 @@ const CreditPopUp =({ setTriggerCreditPopUp, setDownloadAbort }: Props)  => {
                     <CloseIcon/>
                 </IconButton>
             </div>
-
-
             <div className={styles.textContainerStyle}>
                 <h1  className={styles.headerContainerStyle}> Husk kreditering! </h1>
                 <p >
                     Alle bilder tatt av fotogjengen skal krediteres med: <br/><br/>
 
-                
                     Foto: foto.samfundet.no <br/><br/>
 
                     Ved manglende kreditering kan det kreves kompansasjon.  <br/>
                     Har du spørsmål rundt rundt bruk av vår bilder eller kreditering? 
                     Ta kontakt med oss på <i>fg@samfundet.no </i>.<br/><br/>
                 </p>
-
                 {isIntern &&
-                <p>
-                    Husk å ikke dele bilder fra interne områder på sosiale medier! <br/><br/>
-                </p>
+                    <p>
+                        Husk å ikke dele bilder fra interne områder på sosiale medier! <br/><br/>
+                    </p>
                 }
             </div>
-
-
-            
-
             <div className= {styles.buttonContainerStyle}>
                 <Button onClick={() => handleAccept()} className={styles.OKbuttonStyle}>
                     OK!
                 </Button>
-
-
-            </div>
-
-            
+            </div>   
         </Paper>
     )
 };
