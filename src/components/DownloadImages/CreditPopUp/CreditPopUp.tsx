fef33e04181816887492a3ce940cf2d61a8e4652
Mathilde <mathilde.skaset.haar@gmail.com>
2026-02-15 10:59:13 +0100

    Minor styling fixes on text in popup

--- src/components/DownloadImages/CreditPopUp/CreditPopUp.tsx
+++ src/components/DownloadImages/CreditPopUp/CreditPopUp.tsx
@@ -41,13 +41,20 @@ const CreditPopUp =({ setTriggerCreditPopUp, setDownloadAbort }: Props)  => {
 
                     Ved manglende kreditering kan det kreves kompansasjon.  <br/>
                     Har du spørsmål rundt rundt bruk av vår bilder eller kreditering? 
+                    Eller er du presse, kommersielle aktører, eller ønsker å bruke våre bilder uten kreditering?
                     Ta kontakt med oss på <i>fg@samfundet.no </i>.<br/><br/>
+
+                    
                 </p>
                 {isIntern &&
                     <p>
-                        Husk å ikke dele bilder fra interne områder på sosiale medier! <br/><br/>
+                        Husk å ikke dele bilder fra interne områder på sosiale medier!<br/>
                     </p>
                 }
+
+                <p className={styles.textDisclaimerStyle}>
+                        Ved å trykke "OK" aksepterer du Fotogjengens retningslinjer og vilkår for bruk av bilder.
+                </p>
             </div>
             <div className= {styles.buttonContainerStyle}>
                 <Button onClick={() => handleAccept()} className={styles.OKbuttonStyle}>
