580443662856125c538948025be563020dbca91c
Mathilde <mathilde.skaset.haar@gmail.com>
2026-04-15 18:45:39 +0200

    Attempt at hook

---
+++ src/hooks/useDeleteMotive.tsx
@@ -0,0 +1,28 @@
+import { MotiveApi } from "@/utils/api/MotiveApi";
+import { useState } from "react";
+
+export function useDeleteMotive(){
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const deleteMotive = async (id : string) =>{
+
+        setIsLoading(true);
+        setError(null)
+        try{
+            const response = await MotiveApi.deleteById(id)
+            setIsLoading(false)
+            return response;
+        }
+        catch{
+            console.error("Erorr deleting motive:");
+            setIsLoading(false)
+            return null;
+        }
+        finally {
+            setIsLoading(false)
+        }
+    }
+    return {deleteMotive, isLoading, error}
+}
+
