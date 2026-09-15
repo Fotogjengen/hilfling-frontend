c0c4b2557738697dc319524d4ce3e5f41dc4b36b
Markus <markussmadsbakken@gmail.com>
2026-09-15 20:29:51 +0200

    console log er BORTE

--- src/routes/_fgAuthenticated/route.tsx
+++ src/routes/_fgAuthenticated/route.tsx
@@ -3,7 +3,6 @@ import { createFileRoute, notFound, Outlet } from "@tanstack/react-router";
 export const Route = createFileRoute("/_fgAuthenticated")({
   component: Outlet,
   beforeLoad: ({ context }) => {
-    console.log(context.auth.user?.isExternalUser);
     if (
       context.auth.user?.securityLevel !== "FG" ||
       context.auth.user?.isExternalUser
